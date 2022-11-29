40f89c4296dc43da1f4e7be1fe52d5ede51020bb
Alam Sanchez <asancheztorres1905@gmail.com>
2022-11-29 11:37:39 -0600

    Add building type generation

--- src/objects/City.ts
+++ src/objects/City.ts
@@ -8,12 +8,12 @@ import { getObjectSize } from '../utils/math';
 
 export const defaultCityConfig = [
     ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', 'V', 'V', 'V', 'V', 'V', 'V', 'V', '.'],
-    ['.', 'V', '.', '.', 'V', '.', '.', 'V', '.'],
-    ['.', 'V', '.', '.', 'V', 'V', 'V', 'V', '.'],
-    ['.', 'V', 'V', '.', 'V', '.', '.', 'V', '.'],
-    ['.', '.', 'V', 'V', 'V', 'V', 'V', 'S', '.'],
-    ['.', '.', 'V', '.', '.', '.', '.', 'V', '.'],
+    ['.', 'W', 'W', 'W', 'W', 'W', 'W', 'W', '.'],
+    ['.', 'W', '.', '.', 'W', '.', '.', 'W', '.'],
+    ['.', 'W', '.', '.', 'W', 'W', 'W', 'W', '.'],
+    ['.', 'W', 'W', '.', 'W', '.', '.', 'W', '.'],
+    ['.', '.', 'W', 'W', 'W', 'W', 'W', 'S', '.'],
+    ['.', '.', 'W', '.', '.', '.', '.', 'W', '.'],
 ];
 
 const defaultBuildingConfig = {
@@ -38,9 +38,19 @@ export type CityConfig = {
 };
 
 enum CellType {
-    Building,
-    Street,
-    StartPoint,
+    Building = '.',
+    RawStreet = 'V',
+    Street = 'W',
+    StartPoint = 'S',
+    Empty = ' ',
+}
+
+enum BuildingType {
+    Corner,
+    SingleCorner,
+    TwoSides,
+    OneSide,
+    Island,
 }
 
 export class CityLoader {
@@ -84,15 +94,46 @@ export class City {
     buildings: { body: CANNON.Body; object: THREE.Object3D }[] = [];
     buildingMaterial: CANNON.Material = new CANNON.Material('building');
     config: string[][];
+    mapDimensions: { rows: number; cols: number };
+    map: CellType[][];
+    // @ts-ignore
+    startPoint: { x: number; y: number };
     buildingConfig: BuildingConfig = defaultBuildingConfig;
-    modelData: THREE.Object3D;
-    boxes: any[] = [];
+    // modelData: THREE.Object3D;
+    modelData: Map<BuildingType, THREE.Object3D>;
 
     constructor(modelData: THREE.Group, config: string[][]) {
         this.config = config;
+        this.mapDimensions = {
+            rows: this.config.length,
+            cols: this.config[0].length,
+        };
 
-        this.modelData = modelData.getObjectByName('Building')!;
-        let buildingSize = getObjectSize(this.modelData);
+        this.modelData = new Map();
+        this.modelData.set(
+            BuildingType.Corner,
+            modelData.getObjectByName('Building_corner')!
+        );
+        this.modelData.set(
+            BuildingType.Island,
+            modelData.getObjectByName('Building_island')!
+        );
+        this.modelData.set(
+            BuildingType.OneSide,
+            modelData.getObjectByName('Building_one_side')!
+        );
+        this.modelData.set(
+            BuildingType.TwoSides,
+            modelData.getObjectByName('Building_two_sides')!
+        );
+        this.modelData.set(
+            BuildingType.SingleCorner,
+            modelData.getObjectByName('Building_single_corner')!
+        );
+
+        let buildingSize = getObjectSize(
+            this.modelData.get(BuildingType.Island)!
+        );
 
         this.buildingConfig = {
             depth: buildingSize.x,
@@ -100,27 +141,88 @@ export class City {
             width: buildingSize.z,
         };
 
+        this.map = this.validateCityMap();
+
         this.buildCity();
     }
 
-    validateCityMap() {}
+    private getSurroundingStreet(row: number, col: number): number[] {
+        const { rows, cols } = this.mapDimensions;
+        const positions = {
+            up: [1, 0],
+            right: [0, 1],
+            down: [-1, 0],
+            left: [0, -1],
+        };
 
-    genBuilding(
-        depth: number,
-        width: number,
-        startPosition: Vec3,
-        sqCol: number,
-        sqRow: number
-    ) {
-        let x = startPosition.x + depth * sqCol;
-        let y = startPosition.z + width * sqRow;
-        const buildPosition = new Vec3();
-        buildPosition.x = x + depth / 2;
-        buildPosition.z = y + width / 2;
+        const surroundingStreet: number[] = [0, 0, 0, 0]; // up right down left
+        Object.keys(positions).forEach((offset_key, offset_idx) => {
+            // @ts-ignore
+            const offset = positions[offset_key];
+            const i = row + offset[0];
+            const j = col + offset[1];
 
-        this.buildBuilding(buildPosition);
+            if (i < 0 || j < 0 || i >= rows || j >= cols) return;
 
-        return buildPosition;
+            if (
+                this.config[i][j] == CellType.Street ||
+                this.config[i][j] == CellType.StartPoint
+            ) {
+                surroundingStreet[offset_idx] = 1;
+                // surroundingStreet.push(offset_key);
+            }
+        });
+
+        return surroundingStreet;
+    }
+
+    validateCityMap() {
+        const { rows, cols } = this.mapDimensions;
+
+        const newMap: CellType[][] = new Array(rows)
+            .fill(undefined)
+            .map((_) => new Array(cols).fill(CellType.Empty));
+
+        let startPoint: typeof this.startPoint;
+
+        this.config.forEach((row, row_idx) => {
+            row.forEach((cell, col) => {
+                switch (cell) {
+                    case CellType.Building: {
+                        newMap[row_idx][col] = CellType.Building;
+                        break;
+                    }
+                    case CellType.StartPoint: {
+                        if (startPoint) {
+                            throw new Error(
+                                'Map can only have one start point'
+                            );
+                        }
+                        startPoint = { x: row_idx, y: col };
+                        newMap[row_idx][col] = CellType.StartPoint;
+                        break;
+                    }
+                    case CellType.Street: {
+                        newMap[row_idx][col] = CellType.Street;
+                        break;
+                    }
+                    case CellType.Empty: {
+                        newMap[row_idx][col] = CellType.Empty;
+                        break;
+                    }
+                }
+            });
+        });
+
+        // @ts-ignore
+        this.startPoint = startPoint;
+        return newMap;
+    }
+
+    getCellPosition(row: number, col: number): THREE.Vector3 {
+        const { depth, width } = this.buildingConfig;
+
+        return new THREE.Vector3(row * depth, 0, col * width);
     }
 
     getStreetPos() {
@@ -138,17 +240,76 @@ export class City {
 
         this.config.forEach((row, row_idx) => {
             row.forEach((cell, col) => {
-                if (cell == '.') {
-                    this.buildBuilding(
-                        new Vec3(row_idx * depth, 0, col * width)
-                    );
+                switch (cell) {
+                    case CellType.Building: {
+                        const surroundingStreet = this.getSurroundingStreet(
+                            row_idx,
+                            col
+                        );
+                        const count = surroundingStreet.reduce(
+                            (prev, curr) => prev + curr,
+                            0
+                        );
+                        // this.map[row_idx][col] = CellType.Building;
+                        switch (count) {
+                            case 1: {
+                                this.buildBuilding(
+                                    BuildingType.OneSide,
+                                    new Vec3(row_idx * depth, 0, col * width)
+                                );
+                            }
+                            case 2: {
+                                for (let i = 0; i < 4; i++) {
+                                    let next = (i + 1) % 4;
+                                    if (
+                                        surroundingStreet[i] &&
+                                        surroundingStreet[next]
+                                    ) {
+                                        this.buildBuilding(
+                                            BuildingType.Corner,
+                                            new Vec3(
+                                                row_idx * depth,
+                                                0,
+                                                col * width
+                                            )
+                                        );
+                                        break;
+                                    }
+
+                                    if (i == 3) {
+                                        this.buildBuilding(
+                                            BuildingType.TwoSides,
+                                            new Vec3(
+                                                row_idx * depth,
+                                                0,
+                                                col * width
+                                            )
+                                        );
+                                    }
+                                }
+                                break;
+                            }
+                            case 3: {
+                                this.buildBuilding(
+                                    BuildingType.SingleCorner,
+                                    new Vec3(row_idx * depth, 0, col * width)
+                                );
+                            }
+                        }
+
+                        break;
+                    }
                 }
             });
         });
     }
 
-    buildBuilding(position: Vec3) {
-        const model = this.modelData;
+    buildBuilding(
+        buildingType: BuildingType,
+        position: Vec3,
+        rotation: number = 0
+    ) {
+        const model = this.modelData.get(buildingType)!;
 
         // Physics body
         const { depth, height, width } = this.buildingConfig;
@@ -165,12 +326,12 @@ export class City {
             node.castShadow = true;
         });
 
-        // const box = ;
-
-        this.boxes.push(new THREE.BoxHelper(object, 0xff0000));
-
         object.position.set(position.x, position.y, position.z);
         body.position.set(position.x, position.y + height / 2, position.z);
+
+        if (rotation) {
+            object.rotateX(rotation);
+        }
 
         this.buildings.push({
             body,
@@ -184,6 +345,5 @@ export class City {
             scene.add(object);
             object.receiveShadow = true;
         });
-        this.boxes.forEach((box) => scene.add(box));
     }
 }
